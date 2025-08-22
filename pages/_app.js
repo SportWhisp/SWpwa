3f9462d4c8097437c9b79807b3fb81b199108718
Stefano <sen_v89@hotmail.it>
2025-08-22 10:28:19 +0200

    sistemati banner mobile

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import "@/styles/globals.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        id="adsense-lib"
+        strategy="afterInteractive"
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2796727413305777"
+        crossOrigin="anonymous"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
