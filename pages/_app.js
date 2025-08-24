b51b88a0cf1c50433f8fb1397d45689f27c89988
Stefano <sen_v89@hotmail.it>
2025-08-24 18:30:18 +0200

    tornato com'era prima

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,5 @@
 import "@/styles/globals.css";
-import Script from "next/script";
-
-const isProd = process.env.VERCEL_ENV === "production";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      {isProd && (
-        <>
-          {/* Imposta CONSENT MODE default 'denied' prima di AdSense */}
-          <Script id="consent-default" strategy="beforeInteractive">{`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('consent', 'default', {
-              ad_storage: 'denied',
-              ad_user_data: 'denied',
-              ad_personalization: 'denied',
-              analytics_storage: 'denied'
-            });
-          `}</Script>
-
-          <Script
-            id="adsense-lib"
-            strategy="afterInteractive"
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2796727413305777"
-            crossOrigin="anonymous"
-          />
-        </>
-      )}
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,40 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+const isProd = process.env.VERCEL_ENV === "production";
+
 export default function Document() {
   return (
     <Html lang="it">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-        {/* ✅ Favicon */}
         <link rel="icon" href="/Logo.png" type="image/png" />
         <link rel="apple-touch-icon" href="/Logo.png" />
-        
+
+        {isProd && (
+          <>
+            {/* Consent Mode: default 'denied' PRIMA del tag AdSense */}
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('consent','default',{
+                    ad_storage:'denied',
+                    ad_user_data:'denied',
+                    ad_personalization:'denied',
+                    analytics_storage:'denied'
+                  });
+                `,
+              }}
+            />
+            {/* Tag AdSense richiesto da Google (nel <head>) */}
+            <script
+              async
+              src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2796727413305777"
+              crossOrigin="anonymous"
+            ></script>
+          </>
+        )}
       </Head>
       <body>
         <Main />
